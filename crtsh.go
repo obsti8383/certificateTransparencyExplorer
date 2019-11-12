d945c014accaa5332ac95340c591002702b3b747
obsti8383 <github@florian-probst.de>
2019-11-12 20:25:23 +0100

    finalized crt.sh; now using x509 certificates as return values

--- crtsh.go
+++ crtsh.go
@@ -3,93 +3,66 @@
 package main
 
 import (
+	"crypto/x509"
 	"encoding/json"
-	"errors"
+	"encoding/pem"
 	"io/ioutil"
 	"log"
-	"net"
-	"net/http"
-	"time"
+	"strconv"
 )
 
 // Example:
 // {
 //     "issuer_ca_id": 62124,
 //     "issuer_name": "C=US, O=DigiCert Inc, OU=www.digicert.com, CN=Thawte TLS RSA CA G1",
-//     "name_value": "mebp.web.porsche.de",
+//     "name_value": "blablub.example.de",
 //     "min_cert_id": 2086227961,
 //     "min_entry_timestamp": "2019-11-08T10:27:16.191",
 //     "not_before": "2019-11-08T00:00:00",
 //     "not_after": "2021-12-07T12:00:00"
 // }
 type CTEntryCRTSH struct {
-	IssuerDN    string `json:"issuer_name"`
-	subjectName string `json:"name_value"`
-	ValidFrom   string `json:"not_before"`
-	ValidTo     string `json:"not_after"`
+	IssuerDN     string `json:"issuer_name"`
+	SubjectName  string `json:"name_value"`
+	ValidFrom    string `json:"not_before"`
+	ValidTo      string `json:"not_after"`
+	CRTSH_ID     int    `json:"min_cert_id"`
+	ISSUER_CA_ID int    `json:"issuer_ca_id"`
 }
 
-func GetCTEntriesCRTSH(domain string, includeExpired bool) (ctentries []CTEntryCRTSH, err error) {
+func GetCTEntriesCRTSH(domain string, includeExpired bool) (certificates []x509.Certificate, err error) {
 	url := "https://crt.sh/?q=%." + domain + "&output=json"
 	log.Println("url:", url)
-	jsonByteArray, err := getJSONfromCRTSH(url, nil)
+	jsonByteArray, err := getJSONfromWebservice(url, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	//log.Println("jsonByteArray =", string(jsonByteArray))
 	_ = ioutil.WriteFile("crtsh_response.json", jsonByteArray, 0644)
 
+	var ctentries []CTEntryCRTSH
 	err = json.Unmarshal(jsonByteArray, &ctentries)
 
-	// TODO: get complete certificate (as PEM) via: https://crt.sh/?d=2086227961 (min_cert_id)
-	return ctentries, err
-}
-
-func getJSONfromCRTSH(url string, hvals map[string]string) ([]byte, error) {
-	d := net.Dialer{}
-	client := &http.Client{
-		Timeout: 30 * time.Second,
-		Transport: &http.Transport{
-			DialContext:           d.DialContext,
-			MaxIdleConns:          200,
-			IdleConnTimeout:       90 * time.Second,
-			TLSHandshakeTimeout:   10 * time.Second,
-			ExpectContinueTimeout: 5 * time.Second,
-		},
-	}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-
-	req.Header.Add("User-Agent", USER_AGENT)
-	req.Header.Add("Accept", ACCEPT)
-	req.Header.Add("Accept-Language", ACCEPT_LANG)
-	if hvals != nil {
-		for k, v := range hvals {
-			req.Header.Add(k, v)
+	// get complete certificate (as PEM) via: https://crt.sh/?d=2086227961 (min_cert_id)
+	for _, certsh := range ctentries {
+		//log.Println("Getting certificate for: " + certsh.SubjectName + " EndDate: " + certsh.ValidTo)
+		url := "https://crt.sh/?d=" + strconv.Itoa(certsh.CRTSH_ID)
+		log.Println("url:", url)
+		rawCert, err := getJSONfromWebservice(url, nil)
+		if err != nil {
+			return nil, err
 		}
+		//log.Println(string(rawCert))
+
+		derCert, _ := pem.Decode(rawCert)
+		cert, err := x509.ParseCertificate(derCert.Bytes)
+		if err != nil {
+			log.Println(err.Error())
+			return nil, err
+		}
+
+		certificates = append(certificates, *cert)
 	}
 
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	} else if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return nil, errors.New(resp.Status)
-	}
-
-	//log.Println("resp: ", resp)
-
-	in, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-
-	return in, nil
+	return certificates, err
 }
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 // main.go
-// Copyright 2018 Florian Probst.
+// Copyright 2018-2019 Florian Probst.
 // Aufruf mit mehreren Domains aus einer Textdatei via Powershell:
 // (Get-Content .\domains.txt) |  % { invoke-expression  ".\CertificateTransparencyExplorer.exe $_" } >>all.csv
 
@@ -7,7 +7,6 @@ package main
 
 import (
 	"crypto/x509"
-	//"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -16,49 +15,51 @@ import (
 
 func main() {
 	flag.Parse()
+	if len(flag.Args()) < 1 {
+		fmt.Println("Usage: .\\CertificateTransparencyExplorer <dnsname>")
+		fmt.Println("Example: .\\CertificateTransparencyExplorer mydomain.com")
+		return
+	}
 
 	//crt.sh
-	ctentriesCRTSH, err := GetCTEntriesCRTSH(flag.Args()[0], false)
+	certsCRTSH, err := GetCTEntriesCRTSH(flag.Args()[0], false)
 	if err != nil {
 		log.Fatal("Error for " + flag.Args()[0] + ": " + err.Error())
 	}
-	log.Println(ctentriesCRTSH)
 
 	//entrust
-	ctentries, err := GetCTEntries(flag.Args()[0], false)
+	certsEntrust, err := GetCTEntries(flag.Args()[0], false)
 	if err != nil {
 		log.Fatal("Error for " + flag.Args()[0] + ": " + err.Error())
 	}
 
-	sort.Sort(Certificates(ctentries))
+	allCerts := append(certsCRTSH, certsEntrust...)
+
+	sort.Sort(Certificates(allCerts))
 
 	//printHeader()
 	nrOfPreCerts := 0
-	for _, entry := range ctentries {
-		cert, _ := x509.ParseCertificate(entry.Cert)
-		//b, _ := json.MarshalIndent(cert, "", "    ")
+	for _, cert := range allCerts {
 		isPreCertificate := false
 
 		for _, unhandled := range cert.UnhandledCriticalExtensions {
-			//log.Println(unhandled)
-
+			//  1.3.6.1.4.1.11129.2.4.3
 			if unhandled.Equal([]int{1, 3, 6, 1, 4, 1, 11129, 2, 4, 3}) {
-				//log.Printf("Precert found: %s", cert.SerialNumber)
 				isPreCertificate = true
 				nrOfPreCerts++
 			}
-			//  1.3.6.1.4.1.11129.2.4.3
 		}
-		//if !isPreCertificate {
 		printCert(cert, isPreCertificate)
-		//}
 	}
-	log.Println("#log entries:", len(ctentries))
-	log.Println("#precerts:", nrOfPreCerts)
 
+	log.Println("#entrust entries:", len(certsEntrust))
+	log.Println("#crtsh entries:", len(certsCRTSH))
+
+	log.Println("#log entries:", len(allCerts))
+	log.Println("#precerts:", nrOfPreCerts)
 }
 
-func printCert(cert *x509.Certificate, isPreCertificate bool) {
+func printCert(cert x509.Certificate, isPreCertificate bool) {
 	fmt.Printf("%s;%t;%s;%s;\"%s\";%s;%s\n", cert.SerialNumber, isPreCertificate, cert.Issuer.CommonName, cert.Subject.CommonName, cert.DNSNames, cert.NotBefore.String(), cert.NotAfter.String())
 }
 
@@ -66,8 +67,8 @@ func printHeader() {
 	fmt.Printf("Serial Number;Precertificate;Issuer Common Name;Subject Common Name;DNS Names;Valid from;Valid till\n")
 }
 
-type Certificates []CTEntry
+type Certificates []x509.Certificate
 
 func (a Certificates) Len() int           { return len(a) }
 func (a Certificates) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a Certificates) Less(i, j int) bool { return a[i].ValidFrom < a[j].ValidFrom }
+func (a Certificates) Less(i, j int) bool { return a[i].NotBefore.Before(a[j].NotBefore) }
